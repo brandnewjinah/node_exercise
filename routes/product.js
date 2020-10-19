180184e13527b0d003243c870e7c41a209d0e1b1
Jinah <fw7128@gmail.com>
2020-10-19 14:45:49 -0700

    db schema

--- routes/product.js
+++ routes/product.js
@@ -1,23 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const productModel = require("../models/product");
 
 //CRUD - Create, Read, Update, Delete or Get, Post, Put, Delete
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "get data",
-  });
+  productModel
+    .find()
+    .then((docs) => {
+      res.json({
+        msg: "get total product",
+        count: docs.length,
+        products: docs,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        msg: err.message,
+      });
+    });
 });
 
 router.post("/", (req, res) => {
-  const productData = {
+  const newProduct = new productModel({
     name: req.body.productname,
     price: req.body.productprice,
-  };
-  res.json({
-    message: "data created",
-    productInfo: productData,
   });
+
+  newProduct
+    .save()
+    .then((product) => {
+      res.json({
+        msg: "saved product",
+        productInfo: product,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        msg: err.message,
+      });
+    });
 });
 
 router.put("/", (req, res) => {
