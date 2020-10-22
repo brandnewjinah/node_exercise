7ff2f92518a9d76810624f80dd372f331539c3fa
Jinah <fw7128@gmail.com>
2020-10-22 16:43:44 -0700

    set up product router so that it connects to the db

--- ecomm_practice/take3/routes/product.js
+++ ecomm_practice/take3/routes/product.js
@@ -3,15 +3,45 @@ const router = express.Router();
 const productModel = require("../models/product");
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "data retrieved",
-  });
+  //show data from the db
+  productModel
+    .find()
+    .then((products) => {
+      res.json({
+        message: "all products",
+        count: products.length,
+        products: products,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: err.message,
+      });
+    });
 });
 
 router.post("/", (req, res) => {
-  res.json({
-    message: "data created",
+  //from the post req create a new product matching to the model
+  const newProduct = new productModel({
+    name: req.body.productname,
+    category: req.body.productcategory,
+    price: req.body.productprice,
   });
+
+  //with the new product, save to db, then display the product saved with a msg. catch error if any
+  newProduct
+    .save()
+    .then((product) => {
+      res.json({
+        message: "product saved",
+        productInfo: product,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        message: err.message,
+      });
+    });
 });
 
 router.put("/", (req, res) => {
