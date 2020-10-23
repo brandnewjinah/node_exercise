cada93023707ec69dc3eb9d3177684102b4ab0dc
Jinah <fw7128@gmail.com>
2020-10-23 16:09:00 -0700

    user authentication - sign up

--- ecomm_practice/take3/routes/user.js
+++ ecomm_practice/take3/routes/user.js
@@ -5,35 +5,50 @@ const userModel = require("../models/user");
 
 //user sign up
 router.post("/signup", (req, res) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({
+  userModel
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({
+          message: "email already being used",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new userModel({
+              username: req.body.username,
+              email: req.body.email,
+              password: hash,
+            });
+
+            user
+              .save()
+              .then((user) => {
+                console.log(user);
+                res.status(200).json({
+                  message: "user created",
+                  userInfo: user,
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
         error: err,
       });
-    } else {
-      const user = new userModel({
-        username: req.body.username,
-        email: req.body.email,
-        password: hash,
-      });
-
-      user
-        .save()
-        .then((user) => {
-          console.log(user);
-          res.status(200).json({
-            message: "user created",
-            userInfo: user,
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json({
-            error: err,
-          });
-        });
-    }
-  });
+    });
 });
 
 //user log in
